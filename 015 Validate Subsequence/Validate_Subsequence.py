9ba79d97d7a006ad99fef7ac9dc33589d0db006a
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-01 06:29:52 +0600

    Solve 015

--- 015 Validate Subsequence/Validate_Subsequence.py
+++ 015 Validate Subsequence/Validate_Subsequence.py
@@ -1,3 +1,24 @@
+# Solve using while loop
+# time O(n) where n = len(array)
+# space O(1)
 def isValidSubsequence(array, sequence):
-    # Write your code here.
-    pass
+    arr_idx = 0
+    seq_idx = 0
+    while arr_idx<len(array) and seq_idx<len(sequence):
+        if array[arr_idx] == sequence[seq_idx]:
+            seq_idx += 1
+        arr_idx+=1
+    return seq_idx==len(sequence)
+
+
+# Solve using for loop
+# time O(n)
+# space O(1)
+def isValidSubsequence(array, sequence):
+    seq_idx = 0
+    for num in array:
+        if len(sequence) == seq_idx:
+            break
+        if num == sequence[seq_idx]:
+            seq_idx+=1
+    return len(sequence) == seq_idx

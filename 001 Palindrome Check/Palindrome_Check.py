9bd5a98ff790e6eec15e94fc98ac9a4fa9fadcc1
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-01 21:32:47 +0600

    solve 001

--- 001 Palindrome Check/Palindrome_Check.py
+++ 001 Palindrome Check/Palindrome_Check.py
@@ -1,3 +1,11 @@
+# time O(n) - n is len(array)
+# space O(1)
 def isPalindrome(string):
-    # Write your code here.
-    pass
+    left = 0
+    right = len(string)-1
+    while left<right:
+        if not string[left] == string[right]:
+            return False
+        left+=1
+        right-=1
+    return True

d51135bdbd39fbe1ddb3f892095622218a84a09d
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-03 07:43:59 +0600

    Solve 005

--- 005 First Non-Repeating Character/First_Non_Repeating_Character.py
+++ 005 First Non-Repeating Character/First_Non_Repeating_Character.py
@@ -1,3 +1,14 @@
+# time O(n+m) where n is len(string) and m is len(seen)
+# space O(n) - O(25) -> O(1) - because we are only lowercase English-alphabet letters
 def firstNonRepeatingCharacter(string):
-    # Write your code here.
+    seen = {}
+    for idx, ch in enumerate(string):
+        if ch not in seen:
+            seen[ch] = {'firstSeenAt': idx, 'count': 1}
+        else:
+            seen[ch]['count'] += 1
+    for _, item in seen.items():
+        firstSeenAt, count = item.values()
+        if count == 1:
+            return firstSeenAt
     return -1

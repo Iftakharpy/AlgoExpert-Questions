f8245e00fd55ec4a36a9e69f917a4d9b0b9b0147
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-03 06:44:24 +0600

    Solve 004

--- 004 Generate Document/Generate_Document.py
+++ 004 Generate Document/Generate_Document.py
@@ -1,3 +1,19 @@
+# time O(n) where n is len(characters+document)
+# space O(k) where k in number of unique characters in characters
 def generateDocument(characters, document):
-    # Write your code here.
-    return False
+    if document=="":
+        return True
+    chrs = {}
+    for ch in characters:
+        if ch not in chrs:
+            chrs[ch] = 1
+        else:
+            chrs[ch] += 1
+    for ch in document:
+        if ch not in chrs:
+            return False
+        else:
+            chrs[ch]-=1
+            if chrs[ch]<0:
+                return False
+    return True

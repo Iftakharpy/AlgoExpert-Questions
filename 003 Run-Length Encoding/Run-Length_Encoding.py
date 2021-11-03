f6e6050003b8b98a227ad420af2546dadc59a16d
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-03 06:14:10 +0600

    Solve 003

--- 003 Run-Length Encoding/Run-Length_Encoding.py
+++ 003 Run-Length Encoding/Run-Length_Encoding.py
@@ -1,3 +1,23 @@
+# time O(n)
+# space O(n)
 def runLengthEncoding(string):
-    # Write your code here.
-    pass
+    if string=='':
+        return ''
+    enc = []
+    idx = 0
+    count = 0
+    prev_ch = ''
+    while idx<len(string):
+        ch = string[idx]
+        if count == 9:
+            enc.append(f"{count}{prev_ch}")
+            count = 0
+        elif prev_ch!=ch and prev_ch!='':
+            enc.append(f"{count}{prev_ch}")
+            count = 0
+        prev_ch = ch
+        idx+=1
+        count+=1
+    else:
+        enc.append(f"{count}{prev_ch}")
+    return ''.join(enc)

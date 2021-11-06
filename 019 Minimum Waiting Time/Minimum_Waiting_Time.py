55221ec96cb9f9a0b1a450715aefd36396936559
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-06 11:45:17 +0600

    Solve 019

--- 019 Minimum Waiting Time/Minimum_Waiting_Time.py
+++ 019 Minimum Waiting Time/Minimum_Waiting_Time.py
@@ -1,3 +1,24 @@
+# time O(nlogn)
+# space O(1)
 def minimumWaitingTime(queries):
-    # Write your code here.
-    return 0
+    queries.sort()
+    
+    total = 0
+    prev_sum = 0
+    for i in queries[:-1]:
+        prev_sum += i
+        total += prev_sum
+    
+    return total
+
+# time O(nlogn)
+# space O(1)
+def minimumWaitingTime(queries):
+    queries.sort()
+    
+    total = 0
+    for idx, wait_time in enumerate(queries, start=1):
+        queries_left = len(queries) - idx
+        total += queries_left * wait_time
+        
+    return total

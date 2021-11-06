0513557aa9e56cfbe68c3fe71907c9e6f845f9a3
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-06 17:44:59 +0600

    Solve 023

--- 023 Product Sum/Product_Sum.py
+++ 023 Product Sum/Product_Sum.py
@@ -1,5 +1,11 @@
-# Tip: You can use the type(element) function to check whether an item
-# is a list or an integer.
-def productSum(array):
-    # Write your code here.
-    pass
+# time O(n) - n is number of numbers in array including nested arrays
+# space O(d) - d is max depth of arrays (call stack size)
+def productSum(array, depth=1):
+    sum = 0
+    for item in array:
+        if type(item) == list:
+            sum += productSum(item, depth+1)
+        else:
+            sum += item
+    sum *= depth
+    return sum

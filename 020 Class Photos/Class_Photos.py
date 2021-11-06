d9522bb0a48fb78f0839492208547135ca55a7a0
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-06 10:53:04 +0600

    Solve 020

--- 020 Class Photos/Class_Photos.py
+++ 020 Class Photos/Class_Photos.py
@@ -1,3 +1,12 @@
+# time O(nlogn)
+# space O(1)
 def classPhotos(redShirtHeights, blueShirtHeights):
-    # Write your code here.
-    return False
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+    if redShirtHeights[-1]>blueShirtHeights[-1]:
+        redShirtHeights, blueShirtHeights = blueShirtHeights, redShirtHeights
+    
+    for red, blue in zip(redShirtHeights, blueShirtHeights):
+        if not red<blue:
+            return False
+    return True

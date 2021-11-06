87a20538b1a8a25a512164ce3f0043beba5f840b
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-06 14:55:34 +0600

    Solve 025

--- 025 Find Three Largest Numbers/Find_Three_Largest_Numbers.py
+++ 025 Find Three Largest Numbers/Find_Three_Largest_Numbers.py
@@ -1,3 +1,16 @@
+from math import inf
+
+# time O(n)
+# space O(1)
 def findThreeLargestNumbers(array):
-    # Write your code here.
-    pass
+    res = [-inf, -inf, -inf]
+    for num in array:
+        if num >= res[2]:
+            res.append(num)
+            res.pop(0)
+        elif num >= res[1]:
+            res.insert(2, num)
+            res.pop(0)
+        elif num > res[0]:
+            res[0] = num
+    return res

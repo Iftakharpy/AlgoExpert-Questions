4147eb0364b6b843216e5c0d497c630d2d736a94
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-06 20:15:46 +0600

    Solve 011

--- 011 Branch Sums/Branch_Sums.py
+++ 011 Branch Sums/Branch_Sums.py
@@ -6,6 +6,21 @@ class BinaryTree:
         self.right = None
 
 
+# time O(n)
+# space O(n)
+# n is the number of nodes in the BinaryTree
+def recSums(root, sums, sum=0):
+	if root==None:
+		return
+	if root.left is not None:
+		recSums(root.left, sums, sum=sum+root.value)
+	if root.right is not None:
+		recSums(root.right, sums, sum=sum+root.value)
+	if root.left is None and root.right is None:
+		sums.append(sum+root.value)
+	return sums
+
 def branchSums(root):
-    # Write your code here.
-    pass
+	sums = []
+	recSums(root, sums, 0)
+	return sums

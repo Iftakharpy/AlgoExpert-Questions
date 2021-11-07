56f050ab062441c2f073fdc2d284756120a4cafe
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-08 01:22:16 +0600

    Solve 007

--- 007 Insertion Sort/Insertion_Sort.py
+++ 007 Insertion Sort/Insertion_Sort.py
@@ -1,3 +1,13 @@
+# time O(n^2)
+# space O(1)
 def insertionSort(array):
-    # Write your code here.
-    pass
+    for i in range(1, len(array)):
+        for j in range(i, 0, -1):
+            unsorted_first = j
+            sorted_last = j-1
+            if array[unsorted_first]<array[sorted_last]:
+                # Swap
+                array[unsorted_first], array[sorted_last] = array[sorted_last], array[unsorted_first]
+            else:
+                break
+    return array

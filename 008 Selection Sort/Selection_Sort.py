eb019adb1ddc70adb4785f6583aab0b8828f0a7a
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-08 00:22:55 +0600

    Solve 008

--- 008 Selection Sort/Selection_Sort.py
+++ 008 Selection Sort/Selection_Sort.py
@@ -1,3 +1,10 @@
+# time O(n^2)
+# space O(1)
 def selectionSort(array):
-    # Write your code here.
-    pass
+    for swap_with in range(len(array)-1):
+        smallest = swap_with
+        for finder in range(swap_with, len(array)):
+            if array[finder] < array[smallest]:
+                smallest = finder
+        array[swap_with], array[smallest] = array[smallest], array[swap_with]
+    return array

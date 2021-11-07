fc7b0aad95dc6c87120bcaec5b2f9ce9f5337a4d
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-08 04:28:28 +0600

    Add recursive solution

--- 024 Binary Search/Binary_Search.py
+++ 024 Binary Search/Binary_Search.py
@@ -1,3 +1,4 @@
+# Best
 # time O(log(n))
 # space O(1)
 def binarySearch(array, target):
@@ -12,3 +13,25 @@ def binarySearch(array, target):
         else:
             high = mid - 1
     return -1
+
+
+# Good
+# time O(log(n)) - n is len(array)
+# space O(n) - here n is max call stack size
+def binarySearchHelper(array, target, low, high):
+    mid = (low+high)//2
+    # Base cases
+    if array[mid] == target:
+        return mid
+    if low>high:
+        return -1
+    
+    # Recursive case
+    if target>array[mid]:
+        low = mid + 1
+    else:
+        high = mid - 1
+    return binarySearchHelper(array, target, low, high)
+
+def binarySearch(array, target):
+    return binarySearchHelper(array, target, 0, len(array)-1)

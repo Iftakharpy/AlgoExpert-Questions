0f1522845f4add7db6038a4b214092c14d3ec251
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-07 23:43:02 +0600

    Solve 006

--- 006 Bubble Sort/Bubble_Sort.py
+++ 006 Bubble Sort/Bubble_Sort.py
@@ -1,3 +1,8 @@
+# time O(n^2)
+# space O(1)
 def bubbleSort(array):
-    # Write your code here.
-    pass
+    for i in range(len(array)-1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array

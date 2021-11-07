a8acc17a6b09b8b0b3c7c4c8839ef7de585de4b3
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-07 23:45:34 +0600

    Stop early if not swapped

--- 006 Bubble Sort/Bubble_Sort.py
+++ 006 Bubble Sort/Bubble_Sort.py
@@ -2,7 +2,11 @@
 # space O(1)
 def bubbleSort(array):
     for i in range(len(array)-1, 0, -1):
+        is_swapped = False
         for j in range(i):
             if array[j] > array[j+1]:
+                is_swapped=True
                 array[j], array[j+1] = array[j+1], array[j]
+        if not is_swapped:
+            break
     return array

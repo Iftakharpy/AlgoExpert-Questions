4d55b5a85425bbbb00b67709f65480f830a67367
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-08 01:43:48 +0600

    Solve 024

--- 024 Binary Search/Binary_Search.py
+++ 024 Binary Search/Binary_Search.py
@@ -1,3 +1,14 @@
+# time O(log(n))
+# space O(1)
 def binarySearch(array, target):
-    # Write your code here.
-    pass
+    low = 0
+    high = len(array)-1
+    while low<=high:
+        mid = (low+high)//2
+        if array[mid] == target:
+            return mid
+        if target > array[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1

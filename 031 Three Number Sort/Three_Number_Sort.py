08b555ebbd868157bf9e695ee1ca73a871e1a972
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-13 13:16:47 +0600

    Alternative solution for 031

--- 031 Three Number Sort/Three_Number_Sort.py
+++ 031 Three Number Sort/Three_Number_Sort.py
@@ -15,3 +15,27 @@ def threeNumberSort(array, order):
 
 def swap(array, idx_1, idx_2):
 	array[idx_1], array[idx_2] = array[idx_2], array[idx_1]
+
+
+# time O(n)
+# space O(1)
+def threeNumberSort(array, order):
+	first_value = order[0]
+	second_value = order[1]
+	
+	# positions of the numbers in order
+	first_idx, second_idx, third_idx = 0, 0, len(array)-1
+	
+	while second_idx <= third_idx:
+		value = array[second_idx]
+		
+		if value == second_value:
+			second_idx += 1
+		elif value == first_value:
+			swap(array, first_idx, second_idx)
+			first_idx += 1
+			second_idx += 1
+		else:
+			swap(array, second_idx, third_idx)
+			third_idx -= 1
+	return array

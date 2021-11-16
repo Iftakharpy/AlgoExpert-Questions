055f31b6de8f3603bbf9a329f0c87b0fafc68595
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-17 01:01:39 +0600

    Add question 048 category medium

---
+++ 048 Min Heap Construction/Min_Heap_Construction_sandbox.py
@@ -0,0 +1,30 @@
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
+
+import program
+import unittest
+
+
+def isMinHeapPropertySatisfied(array):
+    for currentIdx in range(1, len(array)):
+        parentIdx = (currentIdx - 1) // 2
+        if array[parentIdx] > array[currentIdx]:
+            return False
+    return True
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        minHeap = program.MinHeap([48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41])
+        minHeap.insert(76)
+        self.assertTrue(isMinHeapPropertySatisfied(minHeap.heap))
+        self.assertEqual(minHeap.peek(), -5)
+        self.assertEqual(minHeap.remove(), -5)
+        self.assertTrue(isMinHeapPropertySatisfied(minHeap.heap))
+        self.assertEqual(minHeap.peek(), 2)
+        self.assertEqual(minHeap.remove(), 2)
+        self.assertTrue(isMinHeapPropertySatisfied(minHeap.heap))
+        self.assertEqual(minHeap.peek(), 6)
+        minHeap.insert(87)
+        self.assertTrue(isMinHeapPropertySatisfied(minHeap.heap))

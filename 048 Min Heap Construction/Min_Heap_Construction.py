f1d37d4f4f6c568312bcc64db51f4730869d37aa
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-17 01:03:08 +0600

    Solve 048

---
+++ 048 Min Heap Construction/Min_Heap_Construction.py
@@ -0,0 +1,60 @@
+from math import ceil
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+
+class MinHeap:
+    def __init__(self, array):
+        self.heap = self.buildHeap(array)
+    
+    @classmethod
+    def _get_parent_idx(cls, child_node_idx):
+        return ceil(child_node_idx/2)-1
+    @classmethod
+    def _get_left_child_idx(cls, parent_node_idx):
+        return (parent_node_idx*2)+1
+    @classmethod
+    def _get_right_child_idx(cls, parent_node_idx):
+        return (parent_node_idx*2)+2
+
+    def buildHeap(self, array):
+        for i in range(len(array)):
+            self.siftUp(array, i)
+        return array
+
+    def siftDown(self, heap, parent_idx, max_idx=None):
+        max_idx = max_idx if max_idx is not None else len(heap)-1
+        left_child = self._get_left_child_idx(parent_idx)
+        right_child = self._get_right_child_idx(parent_idx)
+        if left_child>max_idx:
+            return 
+        if right_child<=max_idx:
+            small_idx = left_child if heap[left_child]<heap[right_child] else right_child
+        else:
+            small_idx = left_child
+        if heap[parent_idx]>heap[small_idx]:
+            swap(heap, small_idx, parent_idx)
+            self.siftDown(heap, small_idx, max_idx)
+
+    def siftUp(self, heap, idx):
+        parent_idx = self._get_parent_idx(idx)
+        while  parent_idx>=0 and heap[idx]<heap[parent_idx]:
+            swap(heap, idx, parent_idx)
+            idx = parent_idx
+            parent_idx = self._get_parent_idx(idx)
+
+    def peek(self):
+        return self.heap[0] if self.heap else None
+
+    def remove(self):
+        val = self.heap[0]
+        last_num = self.heap.pop()
+        self.heap[0] = last_num
+        self.siftDown(self.heap, 0, len(self.heap)-1)
+        return val
+        
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp(self.heap, len(self.heap)-1)

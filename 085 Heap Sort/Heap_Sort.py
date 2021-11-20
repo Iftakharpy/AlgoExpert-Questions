1023ab2c303fd53841b280ad01f7f1d97ae4916c
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-20 16:19:06 +0600

    Add time complexities

--- 085 Heap Sort/Heap_Sort.py
+++ 085 Heap Sort/Heap_Sort.py
@@ -17,14 +17,17 @@ def siftDown(array, idx, max_idx=None):
             left_child_idx = (2*idx) + 1
         else:
             return 
-        
+
+# time: O(n) | space: O(1)
 def buildMaxHeap(array):
+    # skip leaf nodes and build max heap
     first_parent_node_idx = len(array)//2
+    # rearrange parent nodes
     for i in range(first_parent_node_idx-1, -1, -1):
         siftDown(array, i, max_idx=len(array)-1)
 
 
-# time: O(nlogn) | space O(1)
+# time: O(n + nlogn) -> O(nlogn) | space O(1)
 def heapSort(array):
     buildMaxHeap(array)
     for i in range(len(array)-1, 0, -1):

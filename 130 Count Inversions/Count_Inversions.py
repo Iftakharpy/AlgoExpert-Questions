ef9b30c25ae600e4427860c605f5aeba737e8500
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-20 16:29:27 +0600

    Remove unnecessary check

--- 130 Count Inversions/Count_Inversions.py
+++ 130 Count Inversions/Count_Inversions.py
@@ -2,7 +2,7 @@
 def countInversions(array):
     inversions = 0
     for i in range(len(array)):
-        for j in range(i, len(array)):
-            if i!=j and array[i] > array[j]:
+        for j in range(i+1, len(array)):
+            if array[i] > array[j]:
                 inversions += 1
     return inversions

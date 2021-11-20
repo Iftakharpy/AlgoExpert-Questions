597a21fed66ac603bd6a41fead08a8ddb165e175
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-20 14:58:12 +0600

    Solve 085

--- 085 Heap Sort/Heap_Sort.py
+++ 085 Heap Sort/Heap_Sort.py
@@ -1,2 +1,33 @@
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+def siftDown(array, idx, max_idx=None):
+    if max_idx is None:
+        max_idx = len(array) - 1
+    left_child_idx = (2*idx) + 1
+    while left_child_idx<=max_idx:
+        right_child_idx = (2*idx) + 2
+        if right_child_idx<=max_idx and array[right_child_idx] > array[left_child_idx]:
+            swap_with_idx = right_child_idx
+        else:
+            swap_with_idx = left_child_idx
+        if array[swap_with_idx] > array[idx]:
+            swap(array, swap_with_idx, idx)
+            idx = swap_with_idx
+            left_child_idx = (2*idx) + 1
+        else:
+            return 
+        
+def buildMaxHeap(array):
+    first_parent_node_idx = len(array)//2
+    for i in range(first_parent_node_idx-1, -1, -1):
+        siftDown(array, i, max_idx=len(array)-1)
+
+
+# time: O(nlogn) | space O(1)
 def heapSort(array):
-    pass
+    buildMaxHeap(array)
+    for i in range(len(array)-1, 0, -1):
+        swap(array, i, 0)
+        siftDown(array, 0, i-1)
+    return array

78c613a8cb782b4aa05e6d404385238af00ee7a4
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-21 15:04:49 +0600

    Add alternative solution

--- 130 Count Inversions/Count_Inversions.py
+++ 130 Count Inversions/Count_Inversions.py
@@ -46,3 +46,42 @@ def mergeSortAndCountInversions(array, low, mid, high):
     for idx, num in enumerate(sorted_sub_array):
         array[low+idx] = num
     return inversions
+
+
+######################################################################################
+# Solution 3 - Same as solution 2 difference is in the implementation of 
+# countSubArrayInversions
+
+# time O(nlogn) | space O(n) - n is len(array)
+def countInversions(array):
+    return countSubArrayInversions(array, 0, len(array)-1)
+
+def countSubArrayInversions(array, low, high):
+    if (high+1-low)<=1:
+        return 0
+    mid = (low+high)//2
+    left_inversions = countSubArrayInversions(array, low, mid)
+    right_inversions = countSubArrayInversions(array, mid+1, high)
+    merging_inversions = mergeSortAndCountInversions(array, low, mid, high)
+    return left_inversions + right_inversions + merging_inversions
+
+def mergeSortAndCountInversions(array, low, mid, high):
+    sorted_sub_array = []
+    inversions = 0
+    
+    left = low
+    right = mid+1
+    while left<=mid and right<=high:
+        if array[right]<array[left]:
+            inversions += mid+1 - left
+            sorted_sub_array.append(array[right])
+            right+=1
+        else:
+            sorted_sub_array.append(array[left])
+            left+=1
+    
+    sorted_sub_array += array[left:mid+1] + array[right:high+1]
+    
+    for idx, num in enumerate(sorted_sub_array):
+        array[low+idx] = num
+    return inversions

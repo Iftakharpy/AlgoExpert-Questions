6a075b60eb7410300c115ba1b034e7607ed7e302
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-26 05:02:39 +0600

    Add time complexities

--- 039 Min Max Stack Construction/Min_Max_Stack_Construction.py
+++ 039 Min Max Stack Construction/Min_Max_Stack_Construction.py
@@ -7,14 +7,17 @@ class Node:
 
 # Feel free to add new properties and methods to the class.
 class MinMaxStack:
+    # time O(1) | space O(1)
     def __init__(self, value=None):
         self.head = None if value is None else Node(value, value, value)
 
+    # time O(1) | space O(1)
     def peek(self):
         if self.head is None:
             return None
         return self.head._value
 
+    # time O(1) | space O(1)
     def pop(self):
         value = self.head._value
         new_head = self.head._next
@@ -22,6 +25,7 @@ class MinMaxStack:
         self.head = new_head
         return value
 
+    # time O(1) | space O(1)
     def push(self, number):
         if self.head is None:
             self.head = Node(number)
@@ -32,11 +36,13 @@ class MinMaxStack:
             node._next = self.head
             self.head = node
 
+    # time O(1) | space O(1)
     def getMin(self):
         if self.head is None:
             return None
         return self.head._min
 
+    # time O(1) | space O(1)
     def getMax(self):
         if self.head is None:
             return None

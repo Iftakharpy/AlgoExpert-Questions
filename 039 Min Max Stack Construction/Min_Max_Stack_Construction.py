f426fdc3a34120af098539d0202c94625eada23a
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-26 04:47:51 +0600

    Solve 039

--- 039 Min Max Stack Construction/Min_Max_Stack_Construction.py
+++ 039 Min Max Stack Construction/Min_Max_Stack_Construction.py
@@ -1,21 +1,43 @@
+class Node:
+    def __init__(self, value, minValue=None, maxValue=None):
+        self._value = value
+        self._min = minValue if minValue is not None else value
+        self._max = maxValue if maxValue is not None else value
+        self._next = None
+
 # Feel free to add new properties and methods to the class.
 class MinMaxStack:
+    def __init__(self, value=None):
+        self.head = None if value is None else Node(value, value, value)
+
     def peek(self):
-        # Write your code here.
-        pass
+        if self.head is None:
+            return None
+        return self.head._value
 
     def pop(self):
-        # Write your code here.
-        pass
+        value = self.head._value
+        new_head = self.head._next
+        del(self.head)
+        self.head = new_head
+        return value
 
     def push(self, number):
-        # Write your code here.
-        pass
+        if self.head is None:
+            self.head = Node(number)
+        else:
+            new_min = min(number, self.head._min)
+            new_max = max(number, self.head._max)
+            node = Node(number, new_min, new_max)
+            node._next = self.head
+            self.head = node
 
     def getMin(self):
-        # Write your code here.
-        pass
+        if self.head is None:
+            return None
+        return self.head._min
 
     def getMax(self):
-        # Write your code here.
-        pass
+        if self.head is None:
+            return None
+        return self.head._max

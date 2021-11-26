f4aef449bfe0ee651d84a92487c3b3bedb3aa739
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-26 22:33:27 +0600

    Add slight variation

--- 046 Remove Kth Node From End/Remove_Kth_Node_From_End.py
+++ 046 Remove Kth Node From End/Remove_Kth_Node_From_End.py
@@ -20,3 +20,23 @@ def removeKthNodeFromEnd(head, k):
         endFinder = endFinder.next
     
     node.next = node.next.next
+
+# time O(n) | space O(1)
+def removeKthNodeFromEnd(head, k):
+    endFinder = head
+    for i in range(k):
+        endFinder = endFinder.next
+    
+    if endFinder is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    
+    # Little variation here
+    node = LinkedList(None)
+    node.next = head
+    while endFinder is not None:
+        node = node.next
+        endFinder = endFinder.next
+    
+    node.next = node.next.next

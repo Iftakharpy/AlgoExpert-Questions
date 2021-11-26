31394ef319f4ec49606761a0b8096c0b6f48c636
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-26 22:26:18 +0600

    Solve 046

--- 046 Remove Kth Node From End/Remove_Kth_Node_From_End.py
+++ 046 Remove Kth Node From End/Remove_Kth_Node_From_End.py
@@ -1,10 +1,22 @@
-# This is an input class. Do not edit.
 class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
 
-
+# time O(n) | space O(1)
 def removeKthNodeFromEnd(head, k):
-    # Write your code here.
-    pass
+    endFinder = head
+    for i in range(k):
+        endFinder = endFinder.next
+    
+    if endFinder is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    
+    node = head
+    while endFinder.next is not None:
+        node = node.next
+        endFinder = endFinder.next
+    
+    node.next = node.next.next

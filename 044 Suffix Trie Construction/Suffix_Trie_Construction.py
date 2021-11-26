e38d9f8ce2a8762e7229b849b5285da09aadf09b
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-26 07:09:28 +0600

    Add question 044 category medium

---
+++ 044 Suffix Trie Construction/Suffix_Trie_Construction.py
@@ -0,0 +1,17 @@
+# Do not edit the class below except for the
+# populateSuffixTrieFrom and contains methods.
+# Feel free to add new properties and methods
+# to the class.
+class SuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.endSymbol = "*"
+        self.populateSuffixTrieFrom(string)
+
+    def populateSuffixTrieFrom(self, string):
+        # Write your code here.
+        pass
+
+    def contains(self, string):
+        # Write your code here.
+        pass
---
+++ 044 Suffix Trie Construction/Suffix_Trie_Construction_sandbox.py
@@ -0,0 +1,18 @@
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
+
+import program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        trie = program.SuffixTrie("babc")
+        expected = {
+            "c": {"*": True},
+            "b": {"c": {"*": True}, "a": {"b": {"c": {"*": True}}}},
+            "a": {"b": {"c": {"*": True}}},
+        }
+        self.assertEqual(trie.root, expected)
+        self.assertTrue(trie.contains("abc"))
